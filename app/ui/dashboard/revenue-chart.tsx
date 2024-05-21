e31a3371ab3ee60ecceb3997c5570ee8542bf58b
aaaaaa <aaaa@aaaa.aaa>
2024-05-21 18:10:38 -0300

    asd

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue } from '@/app/lib/data';
 
@@ -23,7 +23,7 @@ export default async function RevenueChart() {
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
