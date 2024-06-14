56229a6317f41fbbb0e878682548be30dae2acfb
Henrique Moller <hiquesprckk31@gmail.com>
2024-06-14 16:06:45 -0300

    atualização

--- src/routes/SubDashboard/Ajuda.tsx
+++ src/routes/SubDashboard/Ajuda.tsx
@@ -10,16 +10,63 @@ export default function Ajuda() {
     <>
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>Ajuda</h1>
+        <h1 className={`text-4xl`}>Ajuda</h1>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* <CreateInvoice /> */}
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>O que é o sistema?</h1>
       </div>
-      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
-      <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>A Agro Current é um aplicativo de vistoria para máquinas agrícolas, que conta com diversas funcionalidades para auxílio de funcionários.</h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Aba de Usuário</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Local o qual é exibido as informações do usuário logado no momento, para administradores esta aba também pode ser usada para cadastrar novos usuários.</h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Aba de Sensor</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Registrar novos relátorios referentes aos sensores, visualiza-lôs. </h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Aba de Máquinas</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Cadastro de novas máquinas, o usuário pode escolher um dos tipos de máquinas e dar um nome a cada novo item.</h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Aba de Vistorias</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Auxílio de como o processo de vistoria deve ser executado </h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Relátorios</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Registro da checkagem feita antes da entrega e o relátorio depois para da entrega de um maquinário para verificar possiveis avarias.</h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Aba de sair</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>Usada para sair da conta, ao fazer login, sua conta estará disponível para uso por uma hora até ser necessario entrar nela novamente.</h2>
+      </div>
+      <div className="mt-4 flex items-center gap-2 ">
+        <h1 className='font-bold text-2xl'>Quem fez?</h1>
+      </div>
+      <div className="flex items-center gap-2">
+        <h2 className='text-lg'>
+          <ul>
+            <li>Abner</li>
+            <li>Guilherme Moreno</li>
+            <li>Henrique</li>
+            <li>Murilo</li>
+            <li>Sara</li>
+          </ul>
+        </h2>
       </div>
     </div>
     </>
